3d2b533bce71913321defeae85197e76f1663c0a
Titus Wormer <tituswormer@gmail.com>
2023-01-23 13:34:59 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -4,23 +4,27 @@
  * @typedef {Root['children'][number]|Root} Node
  */
 
-import assert from 'node:assert'
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {fromMarkdown} from 'mdast-util-from-markdown'
 import {VFile} from 'vfile'
 import {source} from './index.js'
 
-test('unist-util-source', (t) => {
+test('unist-util-source', () => {
   let file = new VFile('> + **[Hello](./example)**\n> world.')
   /** @type {Node} */
   let node = fromMarkdown(String(file))
 
-  t.equal(source(node, file), '> + **[Hello](./example)**\n> world.', 'root')
+  assert.equal(
+    source(node, file),
+    '> + **[Hello](./example)**\n> world.',
+    'root'
+  )
 
   assert(node.type === 'root')
   node = node.children[0]
   assert(node.type === 'blockquote')
-  t.equal(
+  assert.equal(
     source(node, file),
     '> + **[Hello](./example)**\n> world.',
     'block quote'
@@ -28,31 +32,39 @@ test('unist-util-source', (t) => {
 
   node = node.children[0]
   assert(node.type === 'list')
-  t.equal(source(node, file), '+ **[Hello](./example)**\n> world.', 'list')
+  assert.equal(source(node, file), '+ **[Hello](./example)**\n> world.', 'list')
 
   node = node.children[0]
   assert(node.type === 'listItem')
-  t.equal(source(node, file), '+ **[Hello](./example)**\n> world.', 'list item')
+  assert.equal(
+    source(node, file),
+    '+ **[Hello](./example)**\n> world.',
+    'list item'
+  )
 
   node = node.children[0]
   assert(node.type === 'paragraph')
-  t.equal(source(node, file), '**[Hello](./example)**\n> world.', 'paragraph')
+  assert.equal(
+    source(node, file),
+    '**[Hello](./example)**\n> world.',
+    'paragraph'
+  )
 
   node = node.children[0]
   assert(node.type === 'strong')
-  t.equal(source(node, file), '**[Hello](./example)**', 'strong')
+  assert.equal(source(node, file), '**[Hello](./example)**', 'strong')
 
   node = node.children[0]
   assert(node.type === 'link')
-  t.equal(source(node, file), '[Hello](./example)', 'link')
+  assert.equal(source(node, file), '[Hello](./example)', 'link')
 
   node = node.children[0]
   assert(node.type === 'text')
-  t.equal(source(node, file), 'Hello', 'text')
+  assert.equal(source(node, file), 'Hello', 'text')
 
-  t.equal(source(node.position, file), 'Hello', 'position')
+  assert.equal(source(node.position, file), 'Hello', 'position')
 
-  t.equal(
+  assert.equal(
     source(
       /** @type {Text} */ ({
         type: 'text',
@@ -65,13 +77,13 @@ test('unist-util-source', (t) => {
     'out of bounds data'
   )
 
-  t.equal(
+  assert.equal(
     source(/** @type {Text} */ ({type: 'text', value: 'qwe'}), file),
     null,
     'generated'
   )
 
-  t.equal(source(null, file), null, 'missing')
+  assert.equal(source(null, file), null, 'missing')
 
   file = new VFile('a\r\nb')
   node = fromMarkdown(String(file))
@@ -79,7 +91,7 @@ test('unist-util-source', (t) => {
   node = node.children[0]
   assert(node.type === 'paragraph')
 
-  t.equal(source(node, file), 'a\r\nb', 'cr + lf')
+  assert.equal(source(node, file), 'a\r\nb', 'cr + lf')
 
   file = new VFile('a\rb')
   node = fromMarkdown(String(file))
@@ -87,17 +99,15 @@ test('unist-util-source', (t) => {
   node = node.children[0]
   assert(node.type === 'paragraph')
 
-  t.equal(source(node, file), 'a\rb', 'cr')
+  assert.equal(source(node, file), 'a\rb', 'cr')
 
   file = new VFile('a\n')
   node = fromMarkdown(String(file))
 
-  t.equal(source(node, file), 'a\n', 'eof eol')
+  assert.equal(source(node, file), 'a\n', 'eof eol')
 
   file = new VFile('a\n\rb')
   node = fromMarkdown(String(file))
 
-  t.equal(source(node, file), 'a\n\rb', 'blank lines')
-
-  t.end()
+  assert.equal(source(node, file), 'a\n\rb', 'blank lines')
 })
